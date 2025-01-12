2a7e1248f0f4221077d7ec34f692a513706881a2
RuiNtD <RuiNtD@outlook.com>
2025-01-11 19:38:28 -0700

    Update to Catppuccin Cursors v1.0.2

--- build.ts
+++ build.ts
@@ -1,21 +1,42 @@
 import $, { Path } from "@david/dax";
-import { Uint8ArrayWriter, ZipReader } from "@zip-js/zip-js";
+import { Uint8ArrayWriter, ZipReader, ZipWriter } from "@zip-js/zip-js";
 import * as ini from "@std/ini";
-import { expandGlob } from "@std/fs";
+
+const baseTag = "v1.0.2";
+
+const accents = [
+  "blue",
+  "dark",
+  "flamingo",
+  "green",
+  "lavender",
+  "light",
+  "maroon",
+  "mauve",
+  "peach",
+  "pink",
+  "red",
+  "rosewater",
+  "sapphire",
+  "sky",
+  "teal",
+  "yellow",
+];
+const flavors = ["frappe", "latte", "macchiato", "mocha"];
 
 const cursors = [
   "default.cur", // Normal Select
   "help.cur", // Help Select
   "progress.ani", // Working in Background
   "wait.ani", // Busy
-  "cross.cur", // Precision Select
+  "crosshair.cur", // Precision Select
   "text.cur", // Text Select
   "pencil.cur", // Handwriting
   "not-allowed.cur", // Unavailable
-  "ns-resize.cur", // Vertical Resize
-  "ew-resize.cur", // Horizontal Resize
-  "nwse-resize.cur", // Diagonal Resize 1
-  "nesw-resize.cur", // Diagonal Resize 2
+  "size_ver.cur", // Vertical Resize
+  "size_hor.cur", // Horizontal Resize
+  "size_fdiag.cur", // Diagonal Resize 1
+  "size_bdiag.cur", // Diagonal Resize 2
   "all-scroll.cur", // Move
   "up-arrow.cur", // Alternate Select
   "pointer.cur", // Link Select
@@ -27,16 +48,11 @@ const inCursors = cursors
   .filter(Boolean)
   .map((v) => $.path(v).withExtname("").basename());
 
-async function processFolder(inDir: Path, outDir: Path) {
-  const schemeName = inDir.basename().split("-").slice(0, -1).join(" ");
-
+async function processFolder(inDir: Path, outDir: Path, schemeName: string) {
   await outDir.mkdir();
+  await $`pipx run -q x2wincur ${inCursors} -o ${outDir.resolve()}`.cwd(inDir);
 
-  const cursorsDir = inDir.join("cursors");
-  const inFiles = inCursors.map((v) => cursorsDir.join(v));
-  await $`pipx run -q x2wincur ${inFiles} -o ${outDir}`;
-
-  const cursorDir = `Cursors\\${schemeName.split(" ").join("\\")}`;
+  const cursorDir = `Cursors\\${outDir.basename()}`;
   const regCursors = cursors
     .map((v) => v && `%SYSTEMROOT%\\${cursorDir}\\${v}`)
     .join(",");
@@ -61,45 +77,65 @@ async function processFolder(inDir: Path, outDir: Path) {
     .writeText(ini.stringify(installInf, { pretty: true }));
 }
 
+export function capitalize(str: string) {
+  return str.substring(0, 1).toUpperCase() + str.substring(1);
+}
+
 if (import.meta.main) {
   const temp = $.path("temp");
   await temp.emptyDir();
-  const outdir = $.path("win");
-  await outdir.emptyDir();
+  const outBase = $.path("out");
+  await outBase.emptyDir();
 
-const cursorsDir = $.path("orig/cursors");
   const pb = $.progress("Building", {
-    length: [...cursorsDir.readDirSync()].length,
+    length: flavors.length * accents.length,
   });
   await pb.with(async () => {
-    for await (const zip of expandGlob(cursorsDir.join("*.zip").toString())) {
-      const path = $.path(zip.path);
-      const basename = path.withExtname("").basename();
-      const indir = temp.join(basename);
-      pb.message(basename.replaceAll("-", " "));
+    for (const flavor of flavors)
+      for (const accent of accents) {
+        const flavorName = capitalize(flavor);
+        const accentName = capitalize(accent);
+        const schemeName = `Catppuccin ${flavorName} ${accentName}`;
+        const basename = `catppuccin-${flavor}-${accent}-cursors`;
+        const url = `https://github.com/catppuccin/cursors/releases/download/${baseTag}/${basename}.zip`;
+        pb.message(`${schemeName} Cursors`);
 
-      const zipReader = new ZipReader(await path.open());
-      for await (const entry of zipReader.getEntriesGenerator()) {
-        if (!entry.getData) continue;
+        const zip = $.path("cache").join(baseTag, basename).withExtname(".zip");
+        if (!(await zip.exists())) {
+          await zip.ensureFile();
+          await $.request(url).showProgress().pipeToPath(zip);
+        }
 
-        const uint8 = await entry.getData(new Uint8ArrayWriter());
-        const filePath = temp.join(entry.filename);
+        const zipReader = new ZipReader(await zip.open());
+        for await (const entry of zipReader.getEntriesGenerator()) {
+          if (!entry.getData) continue;
 
-        if (entry.directory) await filePath.ensureDir();
-        else await filePath.write(uint8);
+          const uint8 = await entry.getData(new Uint8ArrayWriter());
+          const filePath = temp.join(entry.filename);
+
+          if (entry.directory) await filePath.ensureDir();
+          else await filePath.write(uint8);
+        }
+
+        const inDir = temp.join(basename, "cursors");
+        const outDir = outBase.join(basename);
+        await processFolder(inDir, outDir, schemeName);
+
+        const zipWriter = new ZipWriter(
+          await outBase
+            .join(`${basename}.zip`)
+            .open({ write: true, create: true })
+        );
+        for await (const file of outDir.readDirFilePaths())
+          await zipWriter.add(
+            `${basename}/${file.basename()}`,
+            await file.open()
+          );
+        await zipWriter.close();
+
+        // outdir.join(basename).remove({ recursive: true });
+        pb.increment();
       }
-      await processFolder(indir, outdir.join(basename));
-
-      // const zipWriter = new ZipWriter(
-      //   await outdir.join(`${basename}.zip`).open({ write: true, create: true })
-      // );
-      // for await (const file of outdir.join(basename).expandGlob("*"))
-      //   await zipWriter.add(`${basename}/${file.name}`, await file.path.open());
-      // await zipWriter.close();
-
-      // outdir.join(basename).remove({ recursive: true });
-      pb.increment();
-    }
   });
-  temp.remove({ recursive: true });
+  await temp.remove({ recursive: true });
 }
